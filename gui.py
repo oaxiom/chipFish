2b7cc8f1bcdfe4349abed14e6bd8865b077215b1
hutchinsa <hutchinsa@localhost.localdomain>
2009-12-03 17:03:31 +0800

    bookmarks work

--- gui.py
+++ gui.py
@@ -17,6 +17,7 @@ import math, sys, time
 import opt, gDraw, glbase_wrapper
 
 from error import *
+from bookmarks import bookmarks
 
 import wx
 from wx import xrc
@@ -37,14 +38,16 @@ from track import track
 
 class cfApp(wx.App):
     def OnInit(self):
+        """
+        (Event)
+
+        Executed on the initialisation of the form.
+        Main entry point for chipFish.
+        """
         # errors should be put into a log;
         #sys.stderr = self # silence errors.
         #sys.stdout = self
         print "chipFish (c) 2009, oAxiom, %s" % (time.strftime("%a, %d %b %Y %H:%M:%S", time.localtime())) # log.
-
-        # set up and load the genome
-        # (In future this will load the initial state here)
-        self.g = glload("data/mm8_refGene.glb")
 
         # load the gui
         self.res = xrc.XmlResource('gui_parent.xrc')
@@ -52,15 +55,13 @@ class cfApp(wx.App):
         # load the mainFrame
         self.main = self.res.LoadFrame(None, "mainFrame")
 
-        # bind the gPanel;
-        sizer = wx.BoxSizer(wx.VERTICAL)
-        self.draw = gDraw.gDraw(self.g)
-        self.gui_panel = wx.xrc.XRCCTRL(self.main, "gui_panel")
-        gDrawPanel = wx.xrc.XRCCTRL(self.main, "gDrawPanel")
-        self.draw.bindPanel(gDrawPanel) # bind the drawPanel to gDraw
-        draw_panel = self.draw.getPanel()
-        sizer.Add(draw_panel, 2, wx.EXPAND, 0) # add the panel to the gui
-        gDrawPanel.SetSizer(sizer) # add it to the GUI
+        # --------------------------------------------------------------
+        # These are user state variables, currently hard coded
+        # but later must go freefrom.
+        # set up and load the genome
+        # (In future this will load the initial state here)
+        self.g = glload("data/mm8_refGene.glb")
+        self.draw = gDraw.gDraw(self.g) # drawer must have access to the genome
 
         self.draw.bindTrack(track(filename="data/NSMash1_new.trk", name="NS5 Mash1 ChIP-seq"))
         self.draw.bindTrack(track(filename="data/SpMash1_new.trk", name="Spinal Cord Mash1 ChIP-seq"))
@@ -79,28 +80,41 @@ class cfApp(wx.App):
         #self.draw.setLocation("16", 91152965, 91156965) # Olig1
         #self.draw.setLocation("5", 140855715, 140873715) # Lnfg
 
+        # end of user hard-coded segments.
+        # --------------------------------------------------------------
+
         # menu names:
         # Boomarks root:
         # bookmarks
         # bookmarkThisLocation ID_BOOKMARK_THIS_LOCATION
         # ID_ORGANISE_BOOKMARKS organiseBookmarks
         # findGene
+        self.__rebind_bookmarks("mm8") # I have to bodge this as self.g has a mangled name.
 
+        # bind the gPanel;
+        sizer = wx.BoxSizer(wx.VERTICAL)
+        self.gui_panel = wx.xrc.XRCCTRL(self.main, "gui_panel")
+        gDrawPanel = wx.xrc.XRCCTRL(self.main, "gDrawPanel")
+        self.draw.bindPanel(gDrawPanel) # bind the drawPanel to gDraw
+        draw_panel = self.draw.getPanel()
+        sizer.Add(draw_panel, 2, wx.EXPAND, 0) # add the panel to the gui
+        gDrawPanel.SetSizer(sizer) # add it to the GUI
 
         # bind events to the GUI.
         self.Bind(wx.EVT_LEFT_DOWN, self._mouseLeftDown, draw_panel)
         self.Bind(wx.EVT_LEFT_UP, self._mouseLeftUp, draw_panel)
-        self.Bind(wx.EVT_BUTTON, self.OnViewLeft, wx.xrc.XRCCTRL(self.main, "butViewLeft"))
-        self.Bind(wx.EVT_BUTTON, self.OnViewRight, wx.xrc.XRCCTRL(self.main, "butViewRight"))
-        self.Bind(wx.EVT_BUTTON, self.OnViewBigRight, wx.xrc.XRCCTRL(self.main, "butViewBigRight"))
-        self.Bind(wx.EVT_BUTTON, self.OnViewBigLeft, wx.xrc.XRCCTRL(self.main, "butViewBigLeft"))
-        self.Bind(wx.EVT_BUTTON, self.OnButZoomIn, wx.xrc.XRCCTRL(self.main, "butZoomIn"))
-        self.Bind(wx.EVT_BUTTON, self.OnButZoomOut, wx.xrc.XRCCTRL(self.main, "butZoomOut"))
-        self.Bind(wx.EVT_BUTTON, self.OnGotoLocationEdit, wx.xrc.XRCCTRL(self.main, "butGoToLoc"))
+        self.Bind(wx.EVT_BUTTON, self.OnViewLeft, xrc.XRCCTRL(self.main, "butViewLeft"))
+        self.Bind(wx.EVT_BUTTON, self.OnViewRight, xrc.XRCCTRL(self.main, "butViewRight"))
+        self.Bind(wx.EVT_BUTTON, self.OnViewBigRight, xrc.XRCCTRL(self.main, "butViewBigRight"))
+        self.Bind(wx.EVT_BUTTON, self.OnViewBigLeft, xrc.XRCCTRL(self.main, "butViewBigLeft"))
+        self.Bind(wx.EVT_BUTTON, self.OnButZoomIn, xrc.XRCCTRL(self.main, "butZoomIn"))
+        self.Bind(wx.EVT_BUTTON, self.OnButZoomOut, xrc.XRCCTRL(self.main, "butZoomOut"))
+        self.Bind(wx.EVT_BUTTON, self.OnGotoLocationEdit, xrc.XRCCTRL(self.main, "butGoToLoc"))
 
         # menu events should get by ID: ID_ABOUT
         self.Bind(wx.EVT_MENU, self.OnMenuAbout, id=xrc.XRCID("about"))
         self.Bind(wx.EVT_MENU, self.OnMenuQuit, id=xrc.XRCID("quit"))
+        self.Bind(wx.EVT_MENU, self.OnMenuAddBookmark, id=xrc.XRCID("bookmarkThisLocation"))
         # get changable elements from the gui and store them locally.
         # (See _updateDisplay())
         self.textGoToLoc = wx.xrc.XRCCTRL(self.main, "textGoToLoc")
@@ -122,33 +136,73 @@ class cfApp(wx.App):
         redrawDisplay will not redraw the gui, it just redraws the
         gDrawPanel
         """
-        self.lastValidLocation = location(chr=self.draw.chromosome, left=self.draw.lbp, right=self.draw.rbp)
+        self.lastValidLocation = self.draw.getLocation()
         if redrawDisplay:
             self.draw.forceRedraw()
         # text boxes.
         self.textGoToLoc.SetValue(str(location(chr=self.draw.chromosome, left=self.draw.lbp, right=self.draw.rbp)))
 
-    def _mouseLeftDown(self, event):
+    def __mouseLeftDown(self, event):
         """
         (Interal)
         Deal with mouse down presses
         """
         pass
 
-    def _mouseLeftUp(self, event):
+    def __mouseLeftUp(self, event):
         """
         (Internal)
         Deal with mouse up presses.
         """
         pass
 
+    def __rebind_bookmarks(self, genome_name):
+        """
+        (Internal)
+        get the bookmarks based on the genome name/identifier
+        and bind them into the bookmarks mend
+        """
+        # get the bookmarks
+        self.__bookmarks = bookmarks(genome_name)
+        self.__bookmark_lookup = {}
+
+        marks = self.__bookmarks.get_all_bookmarks()
+
+        # the menu to load the bookmarks into
+        menu_head = xrc.XRCID("bookmarks")
+        menu_head = xrc.XRCCTRL(self.main, "bookmarks")
+        menu_head = self.main.GetMenuBar().GetMenu(3) # Bookmarks is 3... IS this stable?
+
+        # clear the previous bookmarks:
+        for i in xrange(50000, 50100): # this is a limit to the number of bookmarks...
+            # could you even display 100 bookmarks?
+            try:
+                menu_head.Delete(id=i)
+            except:
+                break # reached the end.
+
+        if not marks:
+            # load a 'None' into the menu
+            menu_head.Append(50000, text="None") # how to make inactive?
+        else:
+            for index, item in enumerate(marks):
+                if index > 100:
+                    break # I don't support more than 100 at the current time...
+                # 50000 + index = ID, well above any other menu ID.
+                menu_head.Append(50000+index, text="%s - (%s)" % (item["loc"], item["notes"]))
+
+                # bind a event
+                self.Bind(wx.EVT_MENU,
+                    self.OnBookmark,
+                    id=50000+index)
+                self.__bookmark_lookup[50000+index] = item["loc"] # maintain a lookback of location versus Id.
+
     #------------------------------------------------------------------
     # Events
 
-    def OnJumpToGenomicLoc(self, event):
-        string_loc = self.textGoToLoc.text
-        print text_loc
-        #self.draw.setLocation(self.
+    def OnBookmark(self, event):
+        # get the parent.
+        self.draw.setLocation(loc=self.__bookmark_lookup[event.GetId()])
         self._updateDisplay()
         event.Skip()
 
@@ -215,4 +269,13 @@ class cfApp(wx.App):
 
         # and die:
         sys.quit()
+
+    def OnMenuAddBookmark(self, event):
+        nearby_features = self.g.getAllDrawableFeaturesInRange(self.draw.getLocation())
+        if len(nearby_features) < 8: # truncate the list if it gets too long.
+            notes = ", ".join([x["name"] for x in nearby_features])
+        else:
+            notes = ", ".join([x["name"] for x in nearby_features[0:7]]+["..."])
+        self.__bookmarks.add_bookmark(self.draw.getLocation(), notes)
+        self.__rebind_bookmarks("mm8") # bodge for now.
 # end of mainFrame
