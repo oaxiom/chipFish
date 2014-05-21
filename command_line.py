35607a2f0bab4308b9b4c2ed28887bbaf836f8e7
oaxiom <oaxiom@localhost>
2014-05-21 09:56:39 +0800

    a few conveniences on the command_line.py version

--- command_line.py
+++ command_line.py
@@ -12,16 +12,15 @@ from app import app
 from glbase_wrapper import location
 
 a = app()
-a.startup(os.path.expanduser("~/Desktop/Projects/coSTAT3/5.ChIP-seq/stat3_trks/stat3_tracks.txt"))
+a.startup(os.path.expanduser("~/Projects/AP2aGCM/trks/track_list.txt"))
 
-a.draw.setLocation(loc=location(loc="chr11:100747467-100813886"))
-a.draw.exportImage("Stat3.svg", type="svg")
+oh = open(os.path.expanduser("~/Projects/AP2aGCM/trks/For raw signal.txt"), "rU")
 
-a.draw.setLocation(loc=location(loc="chr11:117801020-117857062"))
-a.draw.exportImage("Socs3.svg", type="svg")
-
-a.draw.setLocation(loc=location(loc="chr7:20376837-20425079"))
-a.draw.exportImage("Bcl3.svg", type="svg")
-
-a.draw.setLocation(loc=location(loc="chr2:167734770-167805825"))
-a.draw.exportImage("Ptpn1.svg", type="svg")
+for lin in oh:
+    tt = lin.strip().split("\t")
+    
+    print tt
+    if tt[0] != "gname":
+    
+        a.draw.setLocation(loc=location(loc=tt[1]).expand(10000))
+        a.draw.exportImage(os.path.expanduser("~/Projects/AP2aGCM/trks/%s_%s.png" % (tt[0], str(tt[1].replace(":", "-")))), type="png")
