4f25087f45e7b153b8b9e607fa595a85b97cb891
oaxiom <oaxiom@localhost>
2018-02-25 11:15:13 +0800

    glbase3 correct rebase

---
+++ glbase_wrapper/genelist.py
@@ -0,0 +1,77 @@
+"""
+genelist pass through for chipFish, see the accompanying readme.txt for details.
+
+(c) 2009 oAxiom
+
+"""
+
+import sys, os, numpy
+
+sys.path.append("..") # get the parent options
+import opt
+
+sys.path.append(opt.path.glbase_package)
+from . import glbase_stage # import like this to get around namespace issues
+
+class genelist(glbase_stage.genelist):
+    """
+    chipFish override of vanilla glbase
+
+    Adds descriptions for chipFish to interpret the interfaces from
+    """
+    # descriptions, text for localisation
+    __doc__ = "Overriden: Not Present"
+    __tooltype__ = "Vanilla genelist"
+    _default_draw_type = "spot"
+    _available_draw_types = ("bar", "spot")
+
+    # I have to redefine the methods chipFish respects here:
+    def annotate(self, **kargs):
+        glbase_stage.genelist.annotate(self, **kargs)
+
+    # define gui interfaces
+    annotate.tooltype = "Annotate a list of genes"
+    annotate.__gui__ = {"required": {"list": "genelist", "key": "list key", "distance": "int"},
+        "optional": {"resolution": "filename"}} # bind gui descriptor
+
+    def __getitem__(self, key):
+        """
+        Emulate a dict
+        
+        I need to override this, to only return metadata.
+        glbase genome objects return self.qkewyfind[key] by default.
+        """
+        if key == "info": # catch this special key
+            for k in self.meta_data:
+                print(("%s\t:\t%s" % (k, self.meta_data[k])))
+        elif key == "name":
+            return(self.name)
+        else:
+            assert key in self.meta_data, "'%s' not found in this track" % key
+            return(self.meta_data[key])
+
+    # new methods:
+    def get_data(self, type, loc, strand=None, resolution=1, **kargs):
+        """
+        **Purpose**
+            mimic the entry to track.
+
+        **Arguments**
+            loc
+                location span to get the array for.
+
+            strand (Not supported)
+
+            resolution
+                the bp resolution to use for the array.
+
+        **Results**
+            returns a Numpy array.
+        """
+        ret = []
+        if loc["chr"] in self.dataByChr:
+            for item in self.dataByChr[loc["chr"]]:
+                if loc.qcollide(item["loc"]):
+                    ret.append(item["loc"])
+        return(ret)
+
---
+++ glbase_wrapper/genome.py
@@ -0,0 +1,108 @@
+"""
+genome pass through for chipFish, see the accompanying readme.txt for details.
+
+(c) 2009 oAxiom
+
+"""
+
+import sys, os
+
+from .utils import qcollide
+
+sys.path.append("..") # get the parent options
+import opt
+
+sys.path.append(opt.path.glbase_package)
+from . import glbase_stage # import like this to get around namespace issues
+
+class genome(glbase_stage.genome):
+    """
+    chipFish override of vanilla glbase
+
+    Adds descriptions for chipFish to interpret the interfaces from
+    """
+    # descriptions, text for localisation
+    __doc__ = "Overriden: Not Present"
+    __tooltype__ = "Genome"
+    _default_draw_type = "genome"
+    _available_draw_types = ("genome", "genome_stack") # genome_stack not implemented.
+    
+    
+    # extra methods:
+    def get_data(self, type, location):
+        if type == "genome":
+            return(self.getFeatures(location))
+
+    def getAllDrawableFeaturesInRange(self, location):
+        """
+        Historical place-holder, for deletion
+        """
+        return(self.get_data("genome", location))
+
+    def find(self, text, guage_object=None, case_sensitive=False):
+        """
+        **Purpose**
+            find any items containing 'text' in the genelist
+
+        **Arguments**
+            text (string, required)
+                the text string to search for
+
+            guage_object
+                a progress bar of some description with the 
+                method SetRange(). 
+                If None, the progressbar is ignored
+
+            case_sensitive (Optional, default=False)
+                treat the search as case senesitive (or not)
+
+        **Returns**
+            A list of dicts containing the search string
+            modify a progress bar (NotImplemented)
+
+        """
+        ret = []
+
+        if guage_object:
+            guage_object.SetRange(len(self))
+
+        for index, item in enumerate(self.linearData):
+            for key in item:
+                # can only search strings:
+                if case_sensitive:
+                    tstr = str(item[key])
+                else:
+                    tstr = str(item[key]).lower()
+
+                if text in tstr:
+                    ret.append(item)
+                    break # only append once
+            if guage_object:
+                guage_object.SetValue(index)
+
+        return(ret)
+
+    def __getitem__(self, key):
+        """
+        I need to override this, to only return metadata.
+        glbase genome objects return self.qkewyfind[key] by default.
+        """
+        if key == "name":
+            return(self.name)
+
+    # descriptions, text for localisation
+    __doc__ = "Overriden: Not Present"
+    __tooltype__ = "Vanilla genelist"
+
+    # gui stuff.
+
+    # I have to redefine the methods chipFish respects here:
+    def annotate(self, **kargs):
+        glbase_stage.genome.annotate(self, **kargs)
+
+    # define gui interfaces
+    annotate.tooltype = "Annotate a list of genes"
+    annotate.__gui__ = {"required": {"list": "genelist", "key": "list key", "distance": "int"},
+        "optional": {"resolution": "filename"}} # bind gui descriptor
+
+    # next method:
